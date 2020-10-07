f85efa6897c87d02885ef34605985c7c303c81b4
dewy <37771542+iBuyMountainDew@users.noreply.github.com>
2020-10-07 13:00:58 +0100

    initial commit

---
+++ assets/index/script.js
@@ -0,0 +1,34 @@
+const aboutInfo =
+    `<!--About Info-->
+<p>
+    <div>
+      <b><h3>dewy</h3></b>
+      Hi! I'm <b>dewy</b>, a rinky dink Java & Kotlin
+      <br>developer. I specialise in IntelliJ 
+      <br>plugins / IDEs and Minecraft software.
+      <br>
+      <br>I work on <b>KAMI Blue</b>, an open source Forge
+      <br>utility mod for use on anarchy servers.
+      <br>Once I shut down <b><a href="https://dewy.dev/dqs">DQS</a></b> after 3 years of
+      <br>development and operation, I turned my
+      <br>attention towards other projects, including:
+      <br>
+      <br> - <b>MCIDE</b> (IDE for Minecraft developers)
+      <br> - <b>botKit</b> (JVM framework for Minecraft chatbots)
+      <br> - <b>soul</b> (Minecraft utility mod)
+      <br>
+      <br>If you'd like to contact me, use these:
+      <br>
+      <br> - <b>Discord</b> (iBuyMountainDew#3731)
+      <br> - <b>Keybase</b> (dewy)
+      <br> - <b>E-Mail</b> (dewy@dewy.dev)
+      <br>
+      <br>Support my endeavours:
+      <br>
+      <br> - <b><a href="https://paypal.me/dewyredacted">Paypal</a></b>
+      <br> - <b>Bitcoin</b> (13bEqqHzcRL9AouYE2MX8szP7cBavMEJAk)
+      <div><br></div>
+    </div>
+    <br>
+  </p>
+  `;
